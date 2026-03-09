c26153befa30b3137dffc127c9b37ce7e3d736f5
YUR4ST003 <stahivura0@gmail.com>
2026-03-09 13:44:17 +0200

    homework task 07 bug fix04

--- js/task-4.js
+++ js/task-4.js
@@ -2,14 +2,14 @@ let form = document.querySelector('.login-form');
 
 form.addEventListener('submit', event => {
   event.preventDefault();
-  let email = form.elements.email.value;
-  let password = form.elements.password.value;
-  if (email === '' || password.trim() === '') {
+  let email = form.elements.email.value.trim();
+  let password = form.elements.password.value.trim();
+  if (email === '' || password === '') {
     alert('All form fields must be filled in');
   } else {
     let dataObject = {
-      email: email.trim(),
-      password: password.trim(),
+      email: email,
+      password: password,
     };
 
     console.log(dataObject);
